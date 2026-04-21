7f13e0d4377152f25ba924715de7f6dcdaad5bd8
Lee Jia Sheng <leejiasheng0826@gmail.com>
2026-04-21 20:16:46 +0800

    feat: implement LoginRequest and AuthResponse dtos

---
+++ src/main/java/com/js/ticketingsystem/model/dtos/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.js.ticketingsystem.model.dtos;
+
+public record AuthResponse(String token) {
+}
---
+++ src/main/java/com/js/ticketingsystem/model/dtos/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.js.ticketingsystem.model.dtos;
+
+public record LoginRequest(String email, String password) {
+}
