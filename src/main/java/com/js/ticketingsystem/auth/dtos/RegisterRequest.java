99487884c1e8275b32469222d4f2acede20156e6
Lee Jia Sheng <leejiasheng0826@gmail.com>
2026-05-02 14:33:14 +0800

    fix: add validation for dto

--- src/main/java/com/js/ticketingsystem/auth/dtos/RegisterRequest.java
+++ src/main/java/com/js/ticketingsystem/auth/dtos/RegisterRequest.java
@@ -1,9 +1,13 @@
 package com.js.ticketingsystem.auth.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RegisterRequest(
-        String name,
-        String email,
-        String password,
-        String phoneNumber
+        @NotBlank String name,
+        @Email @NotBlank String email,
+        @Size(min = 8) String password,
+        @NotBlank String phoneNumber
 ) {
 }
