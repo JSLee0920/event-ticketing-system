caf3f1f547950343852b0b0c62c95e0223e8e88c
Lee Jia Sheng <leejiasheng0826@gmail.com>
2026-05-04 21:28:14 +0800

    feat: add attributes to category

--- src/main/java/com/js/ticketingsystem/model/entities/Category.java
+++ src/main/java/com/js/ticketingsystem/model/entities/Category.java
@@ -2,12 +2,17 @@ package com.js.ticketingsystem.model.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "categories")
 @Getter
 @Setter
@@ -24,8 +29,18 @@ public class Category {
     @Column(unique = true, nullable = false)
     private String name;
 
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
     @Builder.Default
     @OneToMany(mappedBy = "category")
     private List<Event> events = new ArrayList<>();
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 }
 
