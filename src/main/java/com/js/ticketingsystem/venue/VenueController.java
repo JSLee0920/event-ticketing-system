7564c29134b9297bca1a5663b7bd2285f2627920
Lee Jia Sheng <leejiasheng0826@gmail.com>
2026-05-05 21:04:04 +0800

    feat: implement venue controller

--- src/main/java/com/js/ticketingsystem/venue/VenueController.java
+++ src/main/java/com/js/ticketingsystem/venue/VenueController.java
@@ -1,9 +1,31 @@
 package com.js.ticketingsystem.venue;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.js.ticketingsystem.venue.dtos.VenueRequest;
+import com.js.ticketingsystem.venue.dtos.VenueResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/venue")
 public class VenueController {
+    private final VenueService venueService;
+
+    public VenueController(VenueService venueService) {
+        this.venueService = venueService;
+    }
+
+    @PostMapping
+    public ResponseEntity<VenueResponse> createVenue(@Valid @RequestBody VenueRequest request) {
+        VenueResponse response = venueService.createVenue(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VenueResponse>> getAllVenues() {
+        return ResponseEntity.ok(venueService.getAllVenues());
+    }
 }
