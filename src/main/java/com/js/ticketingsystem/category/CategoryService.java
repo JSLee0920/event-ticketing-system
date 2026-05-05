9a84eff3e6eb698e8af61b5b64615bc7e5da67ac
Lee Jia Sheng <leejiasheng0826@gmail.com>
2026-05-05 10:46:17 +0800

    feat: implement createCategory

--- src/main/java/com/js/ticketingsystem/category/CategoryService.java
+++ src/main/java/com/js/ticketingsystem/category/CategoryService.java
@@ -1,13 +1,35 @@
 package com.js.ticketingsystem.category;
 
+import com.js.ticketingsystem.category.dtos.CategoryRequest;
+import com.js.ticketingsystem.category.dtos.CategoryResponse;
+import com.js.ticketingsystem.model.entities.Category;
 import com.js.ticketingsystem.repository.CategoryRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
-    public CategoryService(CategoryRepository categoryRepository) {
+    public CategoryService(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
         this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @Transactional
+    public CategoryResponse createCategory(CategoryRequest request) {
+        if (categoryRepository.existsByNameIgnoreCase(request.name())) {
+            throw new IllegalArgumentException("This category name already exists!");
+        }
+
+        Category category = Category.builder()
+                .name(request.name())
+                .description(request.description())
+                .build();
+
+        Category savedCategory = categoryRepository.save(category);
+
+        return categoryMapper.toCategoryResponse(savedCategory);
     }
 }
