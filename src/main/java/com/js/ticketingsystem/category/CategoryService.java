a35261ba137b4cd20f6016f9df3c3e7f3f3332a2
Lee Jia Sheng <leejiasheng0826@gmail.com>
2026-05-06 12:36:35 +0800

    feat: add getid endpoints

--- src/main/java/com/js/ticketingsystem/category/CategoryService.java
+++ src/main/java/com/js/ticketingsystem/category/CategoryService.java
@@ -2,12 +2,14 @@ package com.js.ticketingsystem.category;
 
 import com.js.ticketingsystem.category.dtos.CategoryRequest;
 import com.js.ticketingsystem.category.dtos.CategoryResponse;
+import com.js.ticketingsystem.common.ResourceNotFoundException;
 import com.js.ticketingsystem.model.entities.Category;
 import com.js.ticketingsystem.repository.CategoryRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CategoryService {
@@ -40,5 +42,11 @@ public class CategoryService {
                 .stream()
                 .map(categoryMapper::toCategoryResponse)
                 .toList();
+    }
+
+    public CategoryResponse getCategoryById(UUID categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+        return categoryMapper.toCategoryResponse(category);
     }
 }
