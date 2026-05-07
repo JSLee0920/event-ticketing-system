69ffc920a597d59783c15c94af92d703b051fa98
Lee Jia Sheng <leejiasheng0826@gmail.com>
2026-05-07 10:24:24 +0800

    feat: implement getattendeesbyeventid

--- src/main/java/com/js/ticketingsystem/user/UserController.java
+++ src/main/java/com/js/ticketingsystem/user/UserController.java
@@ -3,6 +3,7 @@ package com.js.ticketingsystem.user;
 import com.js.ticketingsystem.user.dtos.UserResponse;
 import com.js.ticketingsystem.user.dtos.UserSummaryResponse;
 import com.js.ticketingsystem.user.dtos.UserUpdateRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -19,23 +20,34 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
-        return ResponseEntity.ok(userService.getUserById(id));
-    }
-
+    // For all logged-in users
     @GetMapping("/me")
     public ResponseEntity<UserResponse> getUserByEmail(@AuthenticationPrincipal(expression = "subject") String email) {
         return ResponseEntity.ok(userService.getUserByEmail(email));
     }
 
+    @PutMapping("/me")
+    public ResponseEntity<UserResponse> updateUser(@AuthenticationPrincipal(expression = "subject") String email, @Valid @RequestBody UserUpdateRequest request) {
+        return ResponseEntity.ok(userService.updateUserProfile(email, request));
+    }
+
+    // For organizer only
+    @GetMapping("/events/{eventId}/attendees")
+    public ResponseEntity<List<UserSummaryResponse>> getEventAttendees(
+            @PathVariable UUID eventId,
+            @AuthenticationPrincipal(expression = "subject") String email
+    ) {
+        return ResponseEntity.ok(userService.getAttendeesByEventId(eventId, email));
+    }
+
+    // Staff Methods Only
     @GetMapping
     public ResponseEntity<List<UserSummaryResponse>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    @PutMapping("/me")
-    public ResponseEntity<UserResponse> updateUser(@AuthenticationPrincipal(expression = "subject") String email, UserUpdateRequest request) {
-        return ResponseEntity.ok(userService.updateUserProfile(email, request));
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 }
--- src/main/java/com/js/ticketingsystem/user/UserService.java
+++ src/main/java/com/js/ticketingsystem/user/UserService.java
@@ -1,11 +1,15 @@
 package com.js.ticketingsystem.user;
 
 import com.js.ticketingsystem.common.ResourceNotFoundException;
+import com.js.ticketingsystem.model.entities.Event;
 import com.js.ticketingsystem.model.entities.User;
+import com.js.ticketingsystem.repository.EventRepository;
 import com.js.ticketingsystem.repository.UserRepository;
 import com.js.ticketingsystem.user.dtos.UserResponse;
 import com.js.ticketingsystem.user.dtos.UserSummaryResponse;
 import com.js.ticketingsystem.user.dtos.UserUpdateRequest;
+import jakarta.transaction.Transactional;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +20,12 @@ import java.util.UUID;
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final EventRepository eventRepository;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, EventRepository eventRepository) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.eventRepository = eventRepository;
     }
 
     // For All Logged-In Users
@@ -29,6 +35,7 @@ public class UserService {
         return userMapper.toUserResponse(user);
     }
 
+    @Transactional
     public UserResponse updateUserProfile(String email, UserUpdateRequest request) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
@@ -45,6 +52,24 @@ public class UserService {
         return userMapper.toUserResponse(savedUser);
     }
 
+    // For organizer only
+    public List<UserSummaryResponse> getAttendeesByEventId(UUID eventId, String organizerEmail) {
+        User organizer = userRepository.findByEmail(organizerEmail)
+                .orElseThrow(() -> new ResourceNotFoundException("Organizer not found"));
+
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found"));
+
+        // Ownership check
+        if (!event.getOrganizer().equals(organizer)) {
+            throw new AccessDeniedException("You can only view attendees for your events");
+        }
+
+        List<User> attendees = userRepository.getAttendeesByEventId(eventId);
+        return attendees.stream().map(userMapper::toUserSummaryResponse).toList();
+    }
+
+    // Staff
     public UserResponse getUserById(UUID userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new BadCredentialsException("User not found"));
